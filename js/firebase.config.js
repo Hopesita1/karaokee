484a021268598f825fa1fca83f59aa20f29c3d8b
Hopesita1 <a20240074@utem.edu.mx>
2026-06-11 15:26:53 -0600

    fix: authDomain correcto

--- js/firebase.config.js
+++ js/firebase.config.js
@@ -1,10 +1,10 @@
 // ── Configuración compartida de Firebase ──
 export const firebaseConfig = {
   apiKey: "AIzaSyC3Kr6BBTZEJ9PSd3Ncjxmc-LjA1P3Dq1w",
-  authDomain: "ojos-verdes-karaoke.firebaseapp.com",
+  authDomain: "ojos-verdes-karaoke-ac092.firebaseapp.com",
   databaseURL: "https://ojos-verdes-karaoke-default-rtdb.firebaseio.com",
   projectId: "ojos-verdes-karaoke",
-  storageBucket: "ojos-verdes-karaoke.firebasestorage.app",
+  storageBucket: "ojos-verdes-karaoke-ac092.firebasestorage.app",
   messagingSenderId: "255639804155",
   appId: "1:255639804155:web:352e475df52aaab904f85e",
   measurementId: "G-HEBTRNP832"
